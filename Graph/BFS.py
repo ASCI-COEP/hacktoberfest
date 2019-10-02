e0887510007ec6d1ae95086a568b81109cd64298
Søren Eriksen <soeren.skov.eriksen@lego.com>
2019-10-02 08:18:46 +0200

    add BFS in python

---
+++ Graph/BFS.py
@@ -0,0 +1,38 @@
+import itertools
+import os
+import socket
+import time
+from Queue import Queue
+from time import sleep
+
+visited = set()
+queue = Queue()
+entry_point = "f"
+queue.put(entry_point)
+operations = 0
+
+graph = {"a": ["c"],
+         "b": ["c", "e"],
+         "c": ["a", "b", "d", "e"],
+         "d": ["c"],
+         "e": ["c", "b"],
+         "f": ["a"]
+         }
+
+# BFS
+while not queue.empty():
+    current_node = queue.get()  # Get next node to process
+    if current_node not in visited:
+        # Add the current node to the set of visited nodes
+        visited.add(current_node)
+        neighbors = graph[current_node]  # Find neighbors
+        # Loop through neighbors and add the newly discovered ones to the queue
+        for neighbor in neighbors:
+            if neighbor not in visited:
+                queue.put(neighbor)
+    operations += 1  # Count operations
+    # elif "AP" in current_node:
+    # print APs.add(current_node)
+
+print("visited:")
+print(visited)
