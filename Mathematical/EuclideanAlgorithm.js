c6da3caec8b16c7b592fa4dd72a995fb788644cb
Damian Sire <damiansire@hotmail.com>
2019-10-09 22:06:25 +0200

    Add extended euclidean algorithm in javascript

--- Mathematical/EuclideanAlgorithm.js
+++ Mathematical/EuclideanAlgorithm.js
@@ -11,3 +11,23 @@ function EuclideanAlgorithmRecursive(a,b){
     return EuclideanAlgorithmRecursive(min, max % min);
 
 }
+
+// ax + by = mcd(a,b)
+
+function EuclideanAlgorithmExtendedRecursive(a, b, x, y){
+
+    if(a == 0)
+    {
+        x = 0;
+        y = 0;
+        return b;
+    }
+
+    let x_i = 1
+    let y_i = 1 
+    let remainder = b%a;
+
+    let GreatestCommonDivisor = EuclideanAlgorithmExtendedRecursive(remainder, a, x_i, y_i) 
+  
+    return GreatestCommonDivisor;
+}
