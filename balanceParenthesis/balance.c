a17dfcc2f3d1b0d499a45dc37fe098dba817ce1d
chaudharirohit2810 <chaudharirohit2810@gmail.com>
2019-10-12 11:31:11 +0530

    C code to check if the entered expression has balanced parenthesis or not using stack

--- balanceParenthesis/balance.c
+++ balanceParenthesis/balance.c
@@ -1,12 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include "stack.h"
-int main() {
+
+int readline(char *arr, int n) {
+	char ch;
+	int count = 0;
+	while((ch = getchar()) != '\n' && count < n)
+		arr[count++] = ch;
+	arr[count] = '\0';
+	return count;
+}
+
+int isBalance(char* s) {
 	stack t;
 	int flag = 1, i = 0;
 	init(&t);
 	char currop;
-	char *s = "{ 2 + 4 * [ 3 / 2]} ";
 	while(s[i] != '\0') {
 		currop = s[i];
 		switch(currop) {
@@ -19,6 +28,9 @@ int main() {
 						flag = 0;
 						break;
 					}
+				}else{
+					flag = 0;
+					break;
 				}
 				break;
 			case ']':
@@ -27,6 +39,9 @@ int main() {
 						flag = 0;
 						break;
 					}
+				}else{
+					flag = 0;
+					break;
 				}
 				break;
 			case '}':
@@ -35,15 +50,29 @@ int main() {
 						flag = 0;
 						break;
 					}
+				}else {
+					flag = 0;
+					break;
 				}
 				break;
 		}
 		i++;
 	}
 	if(flag && isEmpty(&t)){
-		printf("Balanced Parenthesis\n");
+		return 1;
 	}else {
-		printf("Unbalanced Parenthesis\n");
+		return 0;
 	}
-	return 0;
+
+}
+int main() {
+	int x;
+	char *s = (char*) malloc(sizeof(char) * 1024);
+	while(x = readline(s, 1024)) {
+		if(isBalance(s))
+			printf("Expression has balanced parenthesis\n");
+		else
+			printf("Expression has unbalanced parenthesis\n");
+	}
+		return 0;
 }
