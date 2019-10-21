c121c3f1d16a429c06dc565b18cf15c71e803139
PrajwalAdsul <45815474+PrajwalAdsul@users.noreply.github.com>
2019-10-22 00:51:50 +0530

    Create private_constructor.cpp

---
+++ Misc/private_constructor.cpp
@@ -0,0 +1,11 @@
+//Constructor can be private but its object cannot be made
+#include<bits/stdc++.h>
+using namespace std;
+class a{
+    a(){
+        cout << "In constructor\n";
+    }
+};
+int main(){
+    return 0; 
+}
